319f8c5bd6453afea5ce812657b28c9aebaaa71e
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-12 15:13:51 +0200

    Completed first exercise

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,16 +2,17 @@ package org.example;
 
 import com.github.javafaker.Faker;
 import enteties.Customer;
+import enteties.Order;
 import enteties.Product;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Faker faker = new Faker(Locale.ITALY);
 
         Supplier<Product> bookSupplier = () -> new Product(faker.book().title(), "book");
@@ -38,5 +39,36 @@ public class Application {
             productsList.add(babyProductSupplier.get());
             productsList.add(boysProductSupplier.get());
         }
+
+        System.err.println("Lista prodotti:");
+        productsList.forEach(System.out::println);
+
+        TimeUnit.MILLISECONDS.sleep(1500);
+
+        List<Order> orderList = new ArrayList<>();
+        for (Customer customer : customerList) {
+            List<Product> orderProductList = new ArrayList<>();
+            int randomNumberOfOrder=new Random().nextInt(1,4);
+            int counter=0;
+            do {
+                int rnd=new Random().nextInt(1,10);
+                for (int j = 0; j < rnd; j++) {
+                    int n = (int) Math.floor(Math.random() * 40);
+                    orderProductList.add(productsList.get(n));
+                }
+                counter++;
+                orderList.add(new Order(orderProductList, customer));
+            }while (counter<randomNumberOfOrder);
+        }
+
+        System.err.println("Lista ordini:");
+        orderList.forEach(System.out::println);
+
+        TimeUnit.MILLISECONDS.sleep(1500);
+        //***********************************************Esercizio 1*******************************************
+        System.err.println("Numero 1");
+
+        Map<Customer, List<Order>> customerOrders = orderList.stream().collect(Collectors.groupingBy(Order::getCustomer));
+        customerOrders.forEach((customer, orders)-> System.out.println("Customer "+ customer.getName()+ ", orders:"+ orders));
     }
 }
